4455c1b1175deb1b4e038145ae0cb437629f1231
Tim Slot <tim.slot@gmail.com>
2012-05-14 21:32:20 +0200

    setNaam() commentaar toegevoegd constructor(String) commentaar toegevoegd

--- src/classes/Speler.java
+++ src/classes/Speler.java
@@ -11,13 +11,20 @@ public class Speler {
 		
 	}
 	
+	/**
+	 * Constructor waarbij gelijk de naam toegewezen kan worden zonder setNaam() aan te roepen.
+	 * @param naam Naam van de huidige speler
+	 */
 	public Speler(String naam){
 		
 		setNaam( naam );
 		
 	}
 	
-	
+	/**
+	 * Stelt naam in van het huidige object
+	 * @param naam Naam van de huidige speler
+	 */
 	public void setNaam( String naam ) {
 		
 		this.naam = naam;
