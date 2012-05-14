4d3aa2b79f555f8bf67a1f3c588bbe50e2ed242b
Tim Slot <tim.slot@gmail.com>
2012-05-14 21:25:27 +0200

    Functies toegevoegd om een naam op te kunnen geven en te kunnen opvragen.

--- src/classes/Speler.java
+++ src/classes/Speler.java
@@ -2,4 +2,32 @@ package classes;
 
 public class Speler {
 
+	
+	private String naam;
+
+	
+	
+	public Speler() {
+		
+	}
+	
+	public Speler(String naam){
+		
+		setNaam( naam );
+		
+	}
+	
+	
+	public void setNaam( String naam ) {
+		
+		this.naam = naam;
+		
+	}
+	
+	public String getNaam() {
+		
+		return this.naam;
+		
+	}
+	
 }
