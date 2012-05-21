f9b0ebd4fa8854ec72215def9d1cdc1a4085a251
Nanne Wielinga <nanne@mycel.nl>
2012-05-21 10:04:15 +0200

    ImagePanel toegevoegd.

---
+++ src/components/ImagePanel.java
@@ -0,0 +1,24 @@
+package components;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+
+public class ImagePanel extends JPanel{
+
+    private BufferedImage image;
+
+    public ImagePanel(String path) throws IOException {
+        image = ImageIO.read(new File(path));
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        g.drawImage(image, 0, 0, null);
+    }
+
+}
