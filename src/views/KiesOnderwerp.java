2e88ba08e50ba118345b59e275f397aabe7392f4
Nanne Wielinga <nanne@mycel.nl>
2012-05-22 16:28:59 +0200

    Begin van onderwerp kiesscherm.

---
+++ src/views/KiesOnderwerp.java
@@ -0,0 +1,65 @@
+package views;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.GridLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import net.miginfocom.swing.MigLayout;
+import javax.swing.JLabel;
+import java.awt.Font;
+
+import javax.swing.JScrollPane;
+
+public class KiesOnderwerp extends JFrame {
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					KiesOnderwerp frame = new KiesOnderwerp();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public KiesOnderwerp() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 800, 600);
+		getContentPane().setLayout(new MigLayout("", "[grow]", "[84.00][111.00,grow]"));
+		
+		JLabel lblNewLabel = new JLabel("Kies een onderwerp.");
+		lblNewLabel.setFont(new Font("Lucida Grande", Font.PLAIN, 47));
+		getContentPane().add(lblNewLabel, "cell 0 0");
+		
+		JScrollPane scrollPane = new JScrollPane();
+		getContentPane().add(scrollPane, "cell 0 1,grow");
+		
+	    JPanel borderlaoutpanel = new JPanel();
+	    scrollPane.setViewportView(borderlaoutpanel);
+	    borderlaoutpanel.setLayout(new BorderLayout(0, 0));
+
+	    JPanel columnpanel = new JPanel();
+	    borderlaoutpanel.add(columnpanel, BorderLayout.NORTH);
+	    columnpanel.setLayout(new GridLayout(0, 1, 0, 1));
+	    columnpanel.setBackground(Color.gray);
+
+	    for(int i=0;i<10;i++)
+	    {
+	    	views.panels.Onderwerp onderwerp = new views.panels.Onderwerp();
+		    columnpanel.add(onderwerp);
+		}
+	}
+}
---
+++ src/views/panels/Onderwerp.java
@@ -0,0 +1,34 @@
+package views.panels;
+
+import java.io.IOException;
+
+import javax.swing.JPanel;
+
+import components.ImagePanel;
+
+import net.miginfocom.swing.MigLayout;
+import javax.swing.JLabel;
+import java.awt.Font;
+
+public class Onderwerp extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public Onderwerp() {
+		setLayout(new MigLayout("", "[161.00][grow]", "[135.00,grow]"));
+	    try {
+		    ImagePanel image = new ImagePanel("images/stad.jpg");
+		    //image.setBounds(5, 5, 100, 100);
+		    add(image, "cell 0 0");
+		    
+		    JLabel lblHalloWereld = new JLabel("Hallo wereld!");
+		    lblHalloWereld.setFont(new Font("Lucida Grande", Font.PLAIN, 19));
+		    add(lblHalloWereld, "cell 1 0");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
