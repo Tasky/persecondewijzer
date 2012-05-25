0276cd5c6cc4d2f7c9dcc87e52bb1fe5614e36d0
Tim Slot <tim.slot@gmail.com>
2012-05-25 11:07:40 +0200

    getter en setter toegevoegd

--- src/logic/Vraag.java
+++ src/logic/Vraag.java
@@ -1,5 +1,25 @@
 package logic;
 
 public class Vraag {
+	
+	public String vraag;
+	
+	public Vraag( String vraag ) {
+		
+		setVraag(vraag);
+		
+	}
+	
+	public void setVraag ( String vraag ){
+		
+		this.vraag = vraag;
+		
+	}
+	
+	public String getVraag() {
+		
+		return this.vraag;
+		
+	}
 
 }
