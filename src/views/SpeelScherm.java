6be7a92eca2af50e5f08cdc96c4ff69f93b8af9b
Nanne Wielinga <nanne@mycel.nl>
2012-05-30 23:42:32 +0200

    SpeelschermTest hernoemen naar Speelscherm.

--- src/views/SpeelScherm.java
+++ src/views/SpeelScherm.java
@@ -1,25 +1,33 @@
 package views;
 
 import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import net.miginfocom.swing.MigLayout;
-import javax.swing.JLabel;
 import java.awt.Font;
 
-import javax.swing.JPanel;
-import javax.swing.JButton;
-
-import views.components.Icon;
-import views.components.ImagePanel;
-import views.panels.Timer;
-
+import javax.swing.JFrame;
 import java.awt.GridLayout;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.awt.FlowLayout;
-import java.awt.Color;
-public class SpeelScherm extends JFrame {
+
+import javax.swing.JButton;
+import net.miginfocom.swing.MigLayout;
+import javax.swing.JComboBox;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
+import views.components.ImagePanel;
+import javax.swing.JTextPane;
+import java.awt.SystemColor;
+import views.panels.Timer;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JLabel;
+import com.jgoodies.forms.factories.DefaultComponentFactory;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.ScrollPaneConstants;
+
+public class SpeelScherm {
+
+	private JFrame frame;
 
 	/**
 	 * Launch the application.
@@ -28,8 +36,8 @@ public class SpeelScherm extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					SpeelScherm frame = new SpeelScherm();
-					frame.setVisible(true);
+					SpeelScherm window = new SpeelScherm();
+					window.frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -38,27 +46,46 @@ public class SpeelScherm extends JFrame {
 	}
 
 	/**
-	 * Create the frame.
+	 * Create the application.
+	 * @throws IOException 
 	 */
-	public SpeelScherm() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 800, 600);
-		getContentPane().setLayout(new MigLayout("", "[grow]", "[46.00][393.00,grow][grow]"));
+	public SpeelScherm() throws IOException {
+		initialize();
+	}
+
+	int currentCell = 0;
+	/**
+	 * Initialize the contents of the frame.
+	 * @throws IOException 
+	 */
+	private void initialize() throws IOException {
+		frame = new JFrame();
+		frame.getContentPane().setBackground(SystemColor.info);
+		frame.setBounds(300, 300, 1024, 768);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new MigLayout("", "[124][][124,grow][124][][124]", "[110.00][350px:24.00,growprio 90,grow][56.50][126.00,grow,fill]"));
 		
-		JLabel lblWelkeStadHoort = new JLabel("Welke stad hoort er bij dit plaatje?");
-		lblWelkeStadHoort.setFont(new Font("Lucida Grande", Font.PLAIN, 24));
-		getContentPane().add(lblWelkeStadHoort, "cell 0 0");
+		JLabel lblWelkeStadIs = DefaultComponentFactory.getInstance().createTitle("Welke stad is te zien op de afbeelding?");
+		lblWelkeStadIs.setFont(new Font("Tahoma", Font.PLAIN, 47));
+		frame.getContentPane().add(lblWelkeStadIs, "cell 0 0 3 1");
 		
-		JPanel panel_2 = new JPanel();
-		getContentPane().add(panel_2, "cell 0 1,grow");
-		panel_2.setLayout(new MigLayout("", "[142.00,left][grow,leading]", "[grow]"));
+		JPanel buttonsPanel = new JPanel();
+		frame.getContentPane().add(buttonsPanel, "cell 0 1,grow");
+		buttonsPanel.setLayout(new MigLayout("", "[grow]", "[][][][][][][][][]"));
 		
-		JPanel antwoorden = new JPanel();
-		panel_2.add(antwoorden, "cell 0 0,alignx left,growy");
-		antwoorden.setLayout(new MigLayout("", "[45px,fill][25px:25px:25px,grow,right][]", "[16px][][][][][][][][]"));
+		JButton btnStoppen = new JButton("Stop de tijd");
+		frame.getContentPane().add(btnStoppen, "cell 3 2 2 1");
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER);
+		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+		final JPanel panel = new JPanel();
+		frame.getContentPane().add(scrollPane, "cell 0 3 6 1,grow");
+		panel.setLayout(new MigLayout("", "[][][][][][][][][][]", "[grow][]"));
+		scrollPane.setViewportView(panel);
 		
 		
-		ArrayList<String> opties = new ArrayList<String>();
+		final ArrayList<String> opties = new ArrayList<String>();
 		opties.add("Brussel");
 		opties.add("Kopenhagen");
 		opties.add("Berlijn");
@@ -70,45 +97,47 @@ public class SpeelScherm extends JFrame {
 		opties.add("Monaco");
 		
 		int cell = 0;
-		for (String optie : opties) {
-			JLabel label = new JLabel(optie);
-			label.setFont(new Font("Lucida Grande", Font.PLAIN, 16));
-			antwoorden.add(label, "cell 0 "+cell+",alignx left,aligny top");
-			
-			// Bepaalde opties al gekozen maken.. (als voorbeeld)
-			if(cell == 2 || cell == 3 || cell == 6 || cell == 7)
-			{
-				label.setForeground(Color.GRAY);
-				Icon pencilIcon = null;
-				try {
-					pencilIcon = new Icon("pencil");
-				} catch (IOException e1) {
-					// Icoontje foetsie..
-					e1.printStackTrace();
+		
+		final ImagePanel plaatje = new ImagePanel("images/Steden/Brussel.jpg");
+		frame.getContentPane().add(plaatje, "cell 2 1 2 1,grow");
+		plaatje.setAutoResize(true);
+		
+		for (final String optie : opties) {			
+			//antwoorden.add(label, "cell 0 "+cell+",alignx left,aligny top");
+			final JButton button = new JButton(optie);
+			button.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					JComboBox combobox = new JComboBox();
+					DefaultComboBoxModel model = new DefaultComboBoxModel();
+					for(String comboOptie : opties) {
+						model.addElement(comboOptie);
+					}
+					model.setSelectedItem(optie);
+					combobox.setModel(model);
+					combobox.setMaximumRowCount(9);
+					panel.add(combobox, "cell "+currentCell+" 1,growx");
+					frame.validate();
+					
+					ImagePanel imagePanel = null;
+					try {
+						imagePanel = new ImagePanel("images/Steden/"+optie+".jpg");
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					panel.add(imagePanel, "cell "+currentCell+" 0,grow");
+					imagePanel.setAutoResize(true);
+					
+					frame.validate();
+					
+					currentCell++;
+					button.setEnabled(false);
 				}
-				antwoorden.add(pencilIcon, "cell 1 "+cell+",grow");
-			}
+			});
+			buttonsPanel.add(button, "cell 0 "+(cell)+",growx");
 			cell++;
 		}
-		
-		ImagePanel plaatje;
-		try {
-			plaatje = new ImagePanel("images/stad.jpg");
-			panel_2.add(plaatje, "cell 1 0,grow");
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		JPanel panel = new JPanel();
-		getContentPane().add(panel, "cell 0 2,grow");
-		panel.setLayout(new MigLayout("", "[131.00,fill][grow][]", "[49.00,grow,fill]"));
-		
-		JButton btnNewButton = new JButton("Stoppen");
-		panel.add(btnNewButton, "cell 0 0");
-		
-		Timer panel_1 = new Timer();
-		panel.add(panel_1, "cell 2 0,grow");
-		panel_1.setLayout(new GridLayout(0, 1, 0, 0));
+																																		
 	}
+
 }
