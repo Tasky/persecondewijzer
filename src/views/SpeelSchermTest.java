db32e428d0e924f47b5e4b2402c76fe052911496
Nanne Wielinga <nanne@mycel.nl>
2012-05-30 23:41:36 +0200

    Veel aanpassingen aan speelscherm.

--- src/views/SpeelSchermTest.java
+++ src/views/SpeelSchermTest.java
@@ -12,6 +12,7 @@ import javax.swing.JButton;
 import net.miginfocom.swing.MigLayout;
 import javax.swing.JComboBox;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
 import views.components.ImagePanel;
 import javax.swing.JTextPane;
@@ -22,6 +23,7 @@ import javax.swing.JLabel;
 import com.jgoodies.forms.factories.DefaultComponentFactory;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.ScrollPaneConstants;
 
 public class SpeelSchermTest {
 
@@ -61,21 +63,26 @@ public class SpeelSchermTest {
 		frame.getContentPane().setBackground(SystemColor.info);
 		frame.setBounds(300, 300, 1024, 768);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(new MigLayout("", "[124][124][124][124][124][124][124][124][124][124]", "[226.00,grow][24.00,grow][][][][][][][][][56.50][126.00,grow,fill]"));
+		frame.getContentPane().setLayout(new MigLayout("", "[124][][124,grow][124][][124]", "[110.00][350px:24.00,growprio 90,grow][56.50][126.00,grow,fill]"));
 		
 		JLabel lblWelkeStadIs = DefaultComponentFactory.getInstance().createTitle("Welke stad is te zien op de afbeelding?");
 		lblWelkeStadIs.setFont(new Font("Tahoma", Font.PLAIN, 47));
-		frame.getContentPane().add(lblWelkeStadIs, "cell 0 0 8 1");
+		frame.getContentPane().add(lblWelkeStadIs, "cell 0 0 3 1");
 		
-		JButton btnStopDeTijd = new JButton("Stop de Tijd");
-		frame.getContentPane().add(btnStopDeTijd, "cell 8 10");
+		JPanel buttonsPanel = new JPanel();
+		frame.getContentPane().add(buttonsPanel, "cell 0 1,grow");
+		buttonsPanel.setLayout(new MigLayout("", "[grow]", "[][][][][][][][][]"));
 		
+		JButton btnStoppen = new JButton("Stop de tijd");
+		frame.getContentPane().add(btnStoppen, "cell 3 2 2 1");
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER);
+		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
 		final JPanel panel = new JPanel();
-		frame.getContentPane().add(panel, "cell 0 11 9 1,grow");
+		frame.getContentPane().add(scrollPane, "cell 0 3 6 1,grow");
 		panel.setLayout(new MigLayout("", "[][][][][][][][][][]", "[grow][]"));
-		
-		JPanel panel_1 = new JPanel();
-		panel.add(panel_1, "cell 1 0,grow");
+		scrollPane.setViewportView(panel);
 		
 		
 		final ArrayList<String> opties = new ArrayList<String>();
@@ -90,6 +97,10 @@ public class SpeelSchermTest {
 		opties.add("Monaco");
 		
 		int cell = 0;
+		
+		final ImagePanel plaatje = new ImagePanel("images/Steden/Brussel.jpg");
+		frame.getContentPane().add(plaatje, "cell 2 1 2 1,grow");
+		plaatje.setAutoResize(true);
 		
 		for (final String optie : opties) {			
 			//antwoorden.add(label, "cell 0 "+cell+",alignx left,aligny top");
@@ -123,7 +134,7 @@ public class SpeelSchermTest {
 					button.setEnabled(false);
 				}
 			});
-			frame.getContentPane().add(button, "cell 0 "+cell+",growx");
+			buttonsPanel.add(button, "cell 0 "+(cell)+",growx");
 			cell++;
 		}
 																																		
