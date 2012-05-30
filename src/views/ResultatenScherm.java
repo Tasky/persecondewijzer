4deaeaf61c5dcf818dba67cea259b03c44a2d3db
Marcel Krol <marcelkroll@hotmail.com>
2012-05-30 22:49:02 +0200

    Resultaat bijgewerkt

--- src/views/ResultatenScherm.java
+++ src/views/ResultatenScherm.java
@@ -1,6 +1,14 @@
 package views;
 
 import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JEditorPane;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import net.miginfocom.swing.MigLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class ResultatenScherm extends JPanel {
 
@@ -8,6 +16,38 @@ public class ResultatenScherm extends JPanel {
 	 * Create the panel.
 	 */
 	public ResultatenScherm() {
+		
+		JButton btnNewButton = new JButton("Verder");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		
+		JEditorPane dtrpnScore = new JEditorPane();
+		dtrpnScore.setText("Aantal goed: 5 \r\n Aantal fout: 4 \r\n Aantal joker gebruikt: 0 \r\n \r\n Score: 1500 \r\n \r\n Tijd: 90 sec");
+		GroupLayout groupLayout = new GroupLayout(this);
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.TRAILING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap(363, Short.MAX_VALUE)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addComponent(dtrpnScore, GroupLayout.PREFERRED_SIZE, 152, GroupLayout.PREFERRED_SIZE)
+							.addGap(43))
+						.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+							.addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 103, GroupLayout.PREFERRED_SIZE)
+							.addContainerGap())))
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(33)
+					.addComponent(dtrpnScore, GroupLayout.PREFERRED_SIZE, 150, GroupLayout.PREFERRED_SIZE)
+					.addGap(79)
+					.addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 41, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap())
+		);
+		setLayout(groupLayout);
 
 	}
 
