1fda5aec3cd52e51df0d006431ba03ab933d9c34
Tim Slot <tim.slot@gmail.com>
2012-06-04 14:31:52 +0200

    Resolutie aangepast

--- src/views/MainWindow.java
+++ src/views/MainWindow.java
@@ -75,7 +75,7 @@ public class MainWindow {
 		ownPanel = new NicePanel();
 		frame.setContentPane(ownPanel);
 		//frame.getContentPane().setBackground(Color.BLACK);
-		frame.setBounds(100, 100, 1024, 768);
+		frame.setBounds(100, 100, 1280, 720);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(new MigLayout("", "[300.00,grow,left][100.00,grow,fill][100.00,grow][300.00,grow]", "[grow,fill][150px:150px:150px,fill][][][][][][100.00,grow]"));
 		
