f3efb39db8cdfc91cd4dbdda72a7a7943f8fc3e4
Tim Slot <tim.slot@gmail.com>
2012-06-04 12:45:11 +0200

    Onnodige imports verwijderd

--- src/views/MainWindow.java
+++ src/views/MainWindow.java
@@ -13,16 +13,11 @@ import views.components.ImagePanel;
 import views.components.NicePanel;
 
 import java.awt.Container;
-import java.awt.Panel;
 import java.awt.Color;
-import java.awt.Window;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import java.awt.event.FocusEvent;
-import java.awt.event.FocusListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.io.IOException;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
