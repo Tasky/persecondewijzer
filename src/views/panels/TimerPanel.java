5690e2a2c7cd23f55a2c9829636100918a2ee113
Tim Slot <tim.slot@gmail.com>
2012-06-05 19:37:06 +0200

    Timer werkt nu, moet nog wel gekoppeld worden aan de logic class.

--- src/views/panels/TimerPanel.java
+++ src/views/panels/TimerPanel.java
@@ -8,18 +8,25 @@ import javax.swing.JLabel;
 import views.components.Icon;
 
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 import net.miginfocom.swing.MigLayout;
 
 public class TimerPanel extends JPanel {
 
+	private Timer timer;
+	private JLabel lblTime;
+	private ActionListener task;
+	private int seconds = 200;
+	
 	/**
 	 * Create the panel.
 	 */
 	public TimerPanel() {
 		setLayout(new MigLayout("", "[75px:75px,grow,center]", "[55px:55px][48px]"));
 		
-		JLabel lblTime = new JLabel("211");
+		this.lblTime = new JLabel("211");
 		lblTime.setFont(new Font("Lucida Grande", Font.PLAIN, 40));
 		add(lblTime, "cell 0 1,alignx left,aligny top");
 		
@@ -30,6 +37,40 @@ public class TimerPanel extends JPanel {
 			e.printStackTrace();
 		}
 		add(panel, "cell 0 0,grow");
+		
+		this.initTimer(1000);
+		this.startTimer();
 
+	}
+	
+	private void setText ( String seconds ) {
+		
+		lblTime.setText(seconds);
+		
+	} 
+	
+	public void initTimer(int delay) {
+		
+		this.task = new ActionListener() {
+			public void actionPerformed(ActionEvent evt) {
+				setText( Integer.toString(seconds) );
+				seconds--;
+			}
+		};
+		
+		this.timer = new Timer(delay, task);
+		
+	}
+	
+	public void startTimer() {
+		
+		timer.start();
+		
+	}
+	
+	public void stopTimer() {
+		
+		timer.stop();
+		
 	}
 }
