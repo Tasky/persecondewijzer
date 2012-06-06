f238487999165f692413217993c8568453d46d47
Tim Slot <tim.slot@gmail.com>
2012-06-06 15:27:21 +0200

    Create XML files

--- src/data/Content.java
+++ src/data/Content.java
@@ -1,5 +1,33 @@
 package data;
 
-public class Content {
+import org.w3c.dom.*;
+import java.io.File;
 
+public class Content {
+	
+	private Document doc;
+	private File file;
+	private static final String XML_LOCATION = "etc/XML/";
+
+	public Content ( String file ) {
+		
+		createXMLFile(file);
+		
+	}
+	
+	public boolean createXMLFile( String fileName ) {
+		
+		try{
+			this.file = new File( XML_LOCATION + fileName);
+			file.createNewFile();
+		}catch(Exception e) {
+			System.err.println(e);
+			return false;
+		}
+		
+		return true;
+		
+	}
+	
+	
 }
