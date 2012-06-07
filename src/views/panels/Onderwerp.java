41690ca9e437fcf722e747ef92bfc8baebc64c3e
Marcel Krol <marcelkroll@hotmail.com>
2012-06-07 23:46:57 +0200

    OntwerpScherm aangepast

--- src/views/panels/Onderwerp.java
+++ src/views/panels/Onderwerp.java
@@ -8,12 +8,14 @@ import javax.swing.JPanel;
 import net.miginfocom.swing.MigLayout;
 import javax.swing.JLabel;
 
+import views.SpeelScherm;
 import views.components.ImagePanel;
 
 import java.awt.Font;
 import javax.swing.JButton;
 import javax.swing.border.EtchedBorder;
 
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JTabbedPane;
 
@@ -27,12 +29,18 @@ public class Onderwerp extends JPanel {
 		setLayout(new MigLayout("", "[123.00,fill][21.00][408.00,grow][]", "[][100px:135.00:100px,grow,fill][50.00px]"));
 	    try {
 		    
-		    JLabel lblHalloWereld = new JLabel("Steden");
-		    lblHalloWereld.setFont(new Font("Dialog", Font.PLAIN, 24));
-		    add(lblHalloWereld, "cell 0 0");
+		    JLabel lblSteden = new JLabel("Steden");
+		    lblSteden.setFont(new Font("Dialog", Font.PLAIN, 24));
+		    add(lblSteden, "cell 0 0");
+		    
+		    
 		    ImagePanel image = new ImagePanel("images/stad.jpg");
 		    //image.setBounds(5, 5, 100, 100);
 		    add(image, "cell 0 1");
+		    image.setAutoResize(true);
+		    image.setBorder(new EtchedBorder(EtchedBorder.RAISED, null, null));
+			//image.addActionListener(action);
+		    
 		    
 		    JButton btnNewButton = new JButton("Kiezen...");
 		    btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 14));
