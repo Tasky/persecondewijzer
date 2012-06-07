1481f111701378644eab65b45a3e7e6081a44f13
Marcel Krol <marcelkroll@hotmail.com>
2012-06-07 12:23:23 +0200

    Revert "verkeerde class.."
    This reverts commit 113b0e16a534590f190ac6186bbb08a6d6bdb457.

--- src/data/Content.java
+++ src/data/Content.java
@@ -1,8 +1,56 @@
 package data;
 
+import org.w3c.dom.*;
+import java.io.File;
 
 public class Content {
+	
+	private Document doc;
+	private String fileName;
+	private File file;
+	private static final String XML_LOCATION = "etc/XML/";
 
-
+	public Content ( String fileName ) {
+		
+		setFile( fileName );
+		createXMLFile();
+		
+	}
+	
+	private void setFile( String fileName ) {
+		
+		this.fileName = XML_LOCATION + file + ".xml";
+		this.file = new File( this.fileName );
+		
+	}
+	
+	public boolean createXMLFile() {
+		
+		try{
+			file.createNewFile();
+		}catch(Exception e) {
+			System.err.println(e);
+			return false;
+		}
+		
+		return true;
+		
+	}
+	
+	private boolean isFile() {
+		
+		try{
+			file.canRead();
+			file.canWrite();
+			file.isFile();
+		}catch(Exception e){
+			System.err.println(e);
+			return false;
+		}
+		
+		return true;
+		
+	}
+	
 	
 }
