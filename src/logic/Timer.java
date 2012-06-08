83e7387759ca3c0666d583103b3d2d6b88dd3054
Tim Slot <tim.slot@gmail.com>
2012-06-08 20:16:23 +0200

    improved logic.Timer class

--- src/logic/Timer.java
+++ src/logic/Timer.java
@@ -1,26 +1,25 @@
 package logic;
 
+
 public class Timer {
 	
 	private static final int BEGIN_TIJD = 200;
 	private int huidigeTijd;
 	
 	public Timer() {
-		
-		this.huidigeTijd = BEGIN_TIJD;
-		
+		huidigeTijd = BEGIN_TIJD;
 	}
 	
-	public void timerStart() {
-		
-		
-		
+	public boolean hasLost() {
+		return ( huidigeTijd > 0 ) ? true : false;
 	}
 	
-	public void timerStop() {
-		
-		
-		
+	public void setTime( int time ) {
+		huidigeTijd = time;
+	}
+	
+	public int getTime() {
+		return huidigeTijd;
 	}
 	
 
