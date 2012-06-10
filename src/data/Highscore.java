9ce95c4a0f0b3ff878893f58f407701a7f6f541e
Nanne Wielinga <nanne@mycel.nl>
2012-06-10 15:58:15 +0200

    Highscores.

--- src/data/Highscore.java
+++ src/data/Highscore.java
@@ -1,5 +1,68 @@
 package data;
 
-public class Highscore {
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URISyntaxException;
 
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerFactory;
+
+import nu.xom.*;
+
+import exceptions.DataException;
+
+public class Highscore {
+	private Document doc;
+	private Transformer tf;
+	private File file;
+
+	public Highscore() throws DataException {
+		try {
+			file = new File(getClass().getResource("/resource/Highscores.xml").toURI());
+		} catch (URISyntaxException e1) {
+			throw new DataException("Het highscorebestand kan niet worden gevonden.");
+		}
+
+		try {
+			doc = new Builder().build(file);
+		} catch (ValidityException e) {
+			throw new DataException("Fout bij validatie van highscorebestand.\r\n"+e.getMessage());
+		} catch (ParsingException e) {
+			throw new DataException("Fout bij parsen van highscorebestand.\r\n"+e.getMessage());
+		} catch (IOException e) {
+			throw new DataException("Het highscorebestand kan niet worden gevonden.");
+		}
+	}
+	
+	public void getHighscores(int hoeveel) {
+		
+	}
+	
+	public void addHighscore(String spelerNaam, String prijs, String tijdOver) {
+		//"Naam", "Prijzengeld", "Tijd over"
+		Element elHighscore = new Element("highscore");
+		elHighscore.addAttribute(new Attribute("naam", spelerNaam));
+		elHighscore.addAttribute(new Attribute("prijs", prijs));
+		elHighscore.addAttribute(new Attribute("tijdOver", tijdOver));
+		doc.getRootElement().appendChild(elHighscore);
+		String xml = doc.toXML();
+		try {
+			FileWriter fstream = new FileWriter(file);
+			BufferedWriter out = new BufferedWriter(fstream);
+		    out.write(xml);
+		    out.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
 }
---
+++ src/data/testing/HighscoreTester.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package data.testing;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import data.Highscore;
+
+/**
+ * @author nanne
+ *
+ */
+public class HighscoreTester {
+
+	private Highscore highscore;
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		highscore = new data.Highscore();
+	}
+
+	/**
+	 * Test method for {@link data.Highscore#getHighscores(int)}.
+	 */
+	@Test
+	public void testGetHighscores() {
+		//fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link data.Highscore#addHighscore(java.lang.String, java.lang.String, java.lang.String)}.
+	 */
+	@Test
+	public void testAddHighscore() {
+		highscore.addHighscore("Nanne", "10ZILJOEN", "20s");
+//		fail("Not yet implemented"); // TODO
+	}
+
+}
