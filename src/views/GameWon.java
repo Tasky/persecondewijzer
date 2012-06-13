d7cff8b59f880c53aa74cbadfae72cefdb654e7d
Jodi Mettler <jodi.mettler@gmail.com>
2012-06-13 19:29:32 +0200

    Winscherm gemaakt, Gameoverscherm netjes opgemaakt

--- src/views/GameWon.java
+++ src/views/GameWon.java
@@ -11,12 +11,12 @@ import javax.swing.border.EmptyBorder;
 import net.miginfocom.swing.MigLayout;
 import views.components.NicePanel;
 
-public class GameOver extends NicePanel {
+public class GameWon extends NicePanel {
 
 	/**
 	 * Create the frame.
 	 */
-	public GameOver(final Spel spel) {
+	public GameWon(final Spel spel) {
 		setForeground(Color.WHITE);
 		setBounds(0, 0, 800, 543);
 		setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -31,13 +31,13 @@ public class GameOver extends NicePanel {
 		setLayout(new MigLayout("", "[grow][500.00px][grow]",
 				"[80.00][10.00][450.00px,grow][40.00px][11.00,grow]"));
 
-		JLabel lblNewLabel = new JLabel("Game Over");
+		JLabel lblNewLabel = new JLabel("Gewonnen!!");
 		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 56));
 		lblNewLabel.setForeground(Color.WHITE);
 		add(lblNewLabel, "flowy,cell 1 2,alignx center");
 		add(btnTerug, "cell 1 3,grow");
 		
-		JLabel lblDeTijdIs = new JLabel("De tijd is op / je had teveel vragen fout");
+		JLabel lblDeTijdIs = new JLabel("Je hebt 9001 euro gewonnen.");
 		lblDeTijdIs.setFont(new Font("Tahoma", Font.PLAIN, 23));
 		lblDeTijdIs.setForeground(Color.WHITE);
 		add(lblDeTijdIs, "cell 1 2,alignx center");
