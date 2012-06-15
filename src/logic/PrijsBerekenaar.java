a80e5eb2553495e41e18798e73a5e9cc6dfc9d95
Tim Slot <tim.slot@gmail.com>
2012-06-15 20:12:33 +0200

    afgemaakt

--- src/logic/PrijsBerekenaar.java
+++ src/logic/PrijsBerekenaar.java
@@ -5,23 +5,62 @@ import java.util.List;
 public class PrijsBerekenaar {
 	
 	List<Vraag> vragen;
-	int score = 0;
+	int ronde = 0;
+	int score[] = {0, 0, 0, 0};
+	private static final int AANTAL_VRAGEN_PER_RONDE = 9;
+	private static final int MAX_BELASTINGVRIJ = 454;
+	private static final int[] GELD_PER_RONDE = {20, 25, 30, 35};
 	
 	public PrijsBerekenaar() {
 		
 	}
 	
-	public int krijgScore(List<Vraag> vragen) {
+	public void setVragen(List<Vraag> vragen) {
 		this.vragen = vragen;
+	}
+	
+	public int getScore() {
+		int totalScore = 0;
 		
 		parseVragen();
 		
-		return score;
-	}
+		for (int i=0; i<score.length; i++) {
+			totalScore += (score[i]*(i+1*5));
+		}
 		
+		return  totalScore;
+	}
+	
+	public int getGeld() {
+		int bedrag = 0;
+		
+		for (int i=0; i<score.length; i++){
+			//Als er in ronde 4 een fout gemaakt is verliest de gebuiker al zijn geld
+			if (i == 3 && score[i] < AANTAL_VRAGEN_PER_RONDE )
+				return 0;
+			else
+				bedrag += score[i]*GELD_PER_RONDE[i];
+		}
+		
+		//Betaal belasting uit indien nodig
+		if (bedrag > MAX_BELASTINGVRIJ)
+			bedrag = (bedrag/100)*75;
+		
+		return bedrag;
+	}
+	
+	public void setRonde(int rondeNummer) {
+		this.ronde = rondeNummer;
+	}
+	
+	/**
+	 * Telt het aantal goede vragen
+	 */
 	private void parseVragen() {
+		int i=0;
 		for (Vraag vraag : vragen) {
-			this.score += vraag.getScore();
+			this.score[i] = vraag.getScore();
+			i++;
 		}
 	}
 	
