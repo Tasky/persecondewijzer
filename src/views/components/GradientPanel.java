484168d3ab2bca066f3f20c9ab553b0c33d5714d
Nanne Wielinga <nanne@mycel.nl>
2012-06-16 18:22:45 +0200

    NicePanel overlaten erven van een GradientPanel.

---
+++ src/views/components/GradientPanel.java
@@ -0,0 +1,91 @@
+package views.components;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GradientPaint;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+/**
+ * @author nanne
+ *
+ */
+public class GradientPanel extends JPanel {
+	/**
+	 * @param args
+	 */
+	public static void main(final String[] args) {
+		java.awt.EventQueue.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				GradientPanel gradPaintPanel = new GradientPanel();
+				gradPaintPanel.setPreferredSize(new Dimension(400, 300));
+				JFrame frame = new JFrame("GradientPaintEg");
+				frame.getContentPane().add(gradPaintPanel);
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame.pack();
+				frame.setLocationRelativeTo(null);
+				frame.setVisible(true);
+			}
+		});
+	}
+	
+	private Color upper = new Color(255, 255, 255);
+	private Color lower = new Color(0, 0, 0);
+
+	/**
+	 * Open gradientpanel.
+	 */
+	public GradientPanel() {}
+	
+	/**
+	 * @param lower
+	 * @param upper
+	 */
+	public GradientPanel(Color lower, Color upper) {
+		setLowerColor(lower);
+		setUpperColor(upper);
+	}
+	
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		Graphics2D g2 = (Graphics2D) g;
+		GradientPaint gradPaint = new GradientPaint(0, 0, getLowerColor(), 0, getHeight(), getUpperColor());
+		g2.setPaint(gradPaint);
+		g2.fillRect(0, 0, getWidth(), getHeight());
+	}
+
+	/**
+	 * @return the upper
+	 */
+	public Color getUpperColor() {
+		return upper;
+	}
+
+	/**
+	 * @param upper the upper to set
+	 */
+	public void setUpperColor(Color upper) {
+		this.upper = upper;
+	}
+
+	/**
+	 * @return the lower
+	 */
+	public Color getLowerColor() {
+		return lower;
+	}
+
+	/**
+	 * @param lower the lower to set
+	 */
+	public void setLowerColor(Color lower) {
+		this.lower = lower;
+	}
+
+
+}
--- src/views/components/NicePanel.java
+++ src/views/components/NicePanel.java
@@ -9,42 +9,27 @@ import java.awt.Graphics2D;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class NicePanel extends JPanel {
-
-	private static final Color	LIGHT_BLUE	= new Color(41, 117, 200);
-	private static final Color	DARK_BLUE	= new Color(2, 47, 106);
-
-	private static void createAndShowUI() {
-		NicePanel gradPaintPanel = new NicePanel();
-		gradPaintPanel.setPreferredSize(new Dimension(400, 300));
-		JFrame frame = new JFrame("GradientPaintEg");
-		frame.getContentPane().add(gradPaintPanel);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.pack();
-		frame.setLocationRelativeTo(null);
-		frame.setVisible(true);
-	}
-
+public class NicePanel extends GradientPanel {
 	public static void main(String[] args) {
 		java.awt.EventQueue.invokeLater(new Runnable() {
 			@Override
 			public void run() {
-				createAndShowUI();
+				NicePanel gradPaintPanel = new NicePanel();
+				gradPaintPanel.setPreferredSize(new Dimension(400, 300));
+				JFrame frame = new JFrame("GradientPaintEg");
+				frame.getContentPane().add(gradPaintPanel);
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame.pack();
+				frame.setLocationRelativeTo(null);
+				frame.setVisible(true);
 			}
 		});
 	}
-
+	
+	private static final Color	UPPER	= new Color(41, 117, 200);
+	private static final Color	LOWER	= new Color(2, 47, 106);
+	
 	public NicePanel() {
-
+		super(UPPER, LOWER);
 	}
-
-	@Override
-	protected void paintComponent(Graphics g) {
-		super.paintComponent(g);
-		Graphics2D g2 = (Graphics2D) g;
-		GradientPaint gradPaint = new GradientPaint(0, 0, LIGHT_BLUE, 0, getHeight(), DARK_BLUE);
-		g2.setPaint(gradPaint);
-		g2.fillRect(0, 0, getWidth(), getHeight());
-	}
-
 }
