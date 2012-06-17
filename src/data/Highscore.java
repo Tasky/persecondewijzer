95b884c1484c57d452947a202a435b8e9b38430d
Nanne Wielinga <nanne@mycel.nl>
2012-06-17 15:37:52 +0200

    Highscores uitlezen en wegschrijven met een highscore klasse. Sorteren moet nog gebeuren.

--- src/data/Highscore.java
+++ src/data/Highscore.java
@@ -5,12 +5,17 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+
+import logic.Highscore;
 
 import nu.xom.Attribute;
 import nu.xom.Builder;
 import nu.xom.Document;
 import nu.xom.Element;
+import nu.xom.Elements;
 import nu.xom.ParsingException;
 import nu.xom.ValidityException;
 import exceptions.DataException;
@@ -37,12 +42,12 @@ public class Highscore {
 		}
 	}
 
-	public void addHighscore(String spelerNaam, String prijs, String tijdOver) {
+	public void addHighscore(final logic.Highscore score) {
 		// "Naam", "Prijzengeld", "Tijd over"
 		Element elHighscore = new Element("highscore");
-		elHighscore.addAttribute(new Attribute("naam", spelerNaam));
-		elHighscore.addAttribute(new Attribute("prijs", prijs));
-		elHighscore.addAttribute(new Attribute("tijdOver", tijdOver));
+		elHighscore.addAttribute(new Attribute("naam", score.getSpelerNaam()));
+		elHighscore.addAttribute(new Attribute("score", score.getScore()));
+		elHighscore.addAttribute(new Attribute("tijdOver", score.getTijdOver()));
 		doc.getRootElement().appendChild(elHighscore);
 		String xml = doc.toXML();
 		try {
@@ -54,11 +59,27 @@ public class Highscore {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
 	}
-
-	public List<Highscore> getHighscores(int hoeveel) {
-		return null;
-
+	
+	public ArrayList<logic.Highscore> getHighscores(int hoeveel) {
+		Elements elements = doc.getRootElement().getChildElements();
+		
+		ArrayList<logic.Highscore> highscores = new ArrayList<logic.Highscore>();
+		for (int i = 0; i < elements.size(); i++) {
+			Element node = elements.get(i);
+			
+			logic.Highscore highscore = new logic.Highscore();
+			
+			highscore.setSpelerNaam(node.getAttribute("naam").getValue());
+			highscore.setScore(node.getAttribute("score").getValue());
+			highscore.setTijdOver(node.getAttribute("tijdOver").getValue());
+			
+			highscores.add(highscore);
+		}
+		
+		//Collections.sort(highscores, new )
+		//TODO: sorteer highscores
+		
+		return highscores;
 	}
 }
--- src/logic/Highscore.java
+++ src/logic/Highscore.java
@@ -1,5 +1,47 @@
 package logic;
 
+/**
+ * @author nanne
+ *
+ */
 public class Highscore {
-
+	private String spelerNaam;
+	private String score;
+	private String tijdOver;
+	/**
+	 * @return the spelerNaam
+	 */
+	public String getSpelerNaam() {
+		return spelerNaam;
+	}
+	/**
+	 * @param spelerNaam the spelerNaam to set
+	 */
+	public void setSpelerNaam(String spelerNaam) {
+		this.spelerNaam = spelerNaam;
+	}
+	/**
+	 * @return the score
+	 */
+	public String getScore() {
+		return score;
+	}
+	/**
+	 * @param score the score to set
+	 */
+	public void setScore(String score) {
+		this.score = score;
+	}
+	/**
+	 * @return the tijdOver
+	 */
+	public String getTijdOver() {
+		return tijdOver;
+	}
+	/**
+	 * @param tijdOver the tijdOver to set
+	 */
+	public void setTijdOver(String tijdOver) {
+		this.tijdOver = tijdOver;
+	}
 }
