c1c29c1da2e2e8a4d0339110d41028465631a3ca
Nanne Wielinga <nanne@mycel.nl>
2012-06-19 22:55:16 +0200

    Spelregels pimpen.

--- src/views/HelpScherm.java
+++ src/views/HelpScherm.java
@@ -6,6 +6,7 @@ import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.BorderFactory;
 import javax.swing.JEditorPane;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -49,10 +50,19 @@ public class HelpScherm extends NicePanel {
 			}
 		});
 
-		JEditorPane dtrpnHierKomtDe = new JEditorPane();
-		dtrpnHierKomtDe
-				.setText("Ieder spel bestaat uit vier vragen en elke vraag uit negen onderdelen waarvoor een \r\nantwoord moet worden gekozen. Eventueel kan je je antwoorden verbeteren. Je mag niet \r\ntwee keer hetzelfde antwoord geven. Doet Je dat toch, dan geldt het eerste onderdeel als \r\nonbeantwoord (tenzij je alsnog een ander antwoord kiest).\r\n\r\nAls speler heb je een jokersaldo. Bij het begin van de aflevering krijgt je twee jokers. \r\nWorden alle onderdelen van een vraag goed beantwoord \r\n(eventueel door het inzetten van jokers), dan krijgt je er een joker bij.\r\n\r\nNadat de onderdelen beantwoord zijn, kan je jokers inzetten om gemiste of foute \r\nantwoorden af te kopen. Het inzetten van een joker kost 16 seconden speeltijd per stuk. \r\nBovendien moet er wel voldoende jokersaldo zijn.\r\n\r\nJe moet minimaal 5 van de 9 onderdelen goed hebben om door te gaan naar de volgende ronde. Het totaalbedrag dat gewonnen kan worden varieert afhankelijk van het spelverloop. Je ontvangt een bedrag zodra je na een ronde stopt.");
-		dtrpnHierKomtDe.setEditable(false);
+		JLabel spelregels = new JLabel();
+		spelregels.setBorder(BorderFactory.createLineBorder(Color.white, 10));
+		spelregels.setBackground(Color.white);
+		spelregels.setOpaque(true);
+		spelregels
+				.setText("<html><p>Ieder spel bestaat uit vier vragen en elke vraag uit negen onderdelen waarvoor een antwoord moet worden gekozen. Eventueel kan je je antwoorden verbeteren. Je mag niet twee keer hetzelfde antwoord geven. Doet Je dat toch, dan geldt het eerste onderdeel als onbeantwoord (tenzij je alsnog een ander antwoord kiest).</p><br />" +
+						"<p>Als speler heb je een jokersaldo. Bij het begin van de aflevering krijgt je twee jokers. Worden alle onderdelen van een vraag goed beantwoord (eventueel door het inzetten van jokers), dan krijgt je er een joker bij.</p><br />" +
+						"<p>Nadat de onderdelen beantwoord zijn, kan je jokers inzetten om gemiste of foute antwoorden af te kopen. Het inzetten van een joker kost 16 seconden speeltijd per stuk. Bovendien moet er wel voldoende jokersaldo zijn.</p><br />" +
+						"<p>Je moet minimaal 5 van de 9 onderdelen goed hebben om door te gaan naar de volgende ronde. Het totaalbedrag dat gewonnen kan worden varieert afhankelijk van het spelverloop. Je ontvangt een bedrag zodra je na een ronde stopt.</p></html>");
+
+
+
+
 		setLayout(new MigLayout("", "[grow][500.00px][grow]", "[80.00][10.00][450.00px,grow][40.00px][11.00,grow]"));
 
 		JLabel lblHelp = new JLabel("Spelregels");
@@ -60,6 +70,6 @@ public class HelpScherm extends NicePanel {
 		lblHelp.setFont(new Font("Dialog", Font.PLAIN, 40));
 		add(lblHelp, "cell 1 0,alignx center,aligny center");
 		add(btnTerug, "cell 1 3,grow");
-		add(dtrpnHierKomtDe, "cell 1 2,grow");
+		add(spelregels, "cell 1 2,grow");
 	}
 }
